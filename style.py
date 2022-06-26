9b173598005aaa10c70ad90a2cc8a59098c2f64e
TEAM ASTERY-X <91943145+TeamAsteryX@users.noreply.github.com>
2022-06-26 14:55:10 +0530

    Create style.py

---
+++ style.py
@@ -0,0 +1,21 @@
+
+bullets = {
+    "bullet1": ">",
+    "bullet2": "•",
+    "bullet3": "⋟",
+    "bullet4": "◈",
+    "bullet5": "┏",
+    "bullet6": "┣",
+    "bullet7": "┗",
+}  # used in .ls - cloud_tools.py, .ci & .ui - chat_utils.py and in .listsudo - sudo_manager.py
+
+alive_format = {
+    # tbd
+}  # used in .alive - alive.py
+
+ping_format = {
+    "ping_emoji1": "🏓",
+    "ping_emoji2": "⚙️",
+}  # used in .ping - ping.py UserBot & Bot and in inline restart
+
+def_emoji = {"emoji": "😁"}  # used in .kang - stcr.py
