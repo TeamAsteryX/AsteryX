9ee76fc6155b17c6a2c8b8b61dbafa6f1c5ac162
TEAM ASTERY-X <91943145+TeamAsteryX@users.noreply.github.com>
2022-06-26 15:05:08 +0530

    Create __init__.py

---
+++ tests/__init__.py
@@ -0,0 +1,7 @@
+# Copyright (C) 2021-present by TeamAsteryX@Github, < https://github.com/TeamAsteryX >.
+#
+# This file is part of < https://github.com/TeamAsteryX/Asteryx > project,
+# and is released under the "GNU v3.0 License Agreement".
+# Please see < https://github.com/TeamAsteryX/Asteryx/blob/main/LICENSE >
+#
+# All rights reserved.
